a815eee6c87ecb670a4ec887ec4bcd10e855b06c
emilyAlvarado <emilyjhas@gmail.com>
2021-06-17 22:55:37 -0500

    por finnn

---
+++ PARTE INDIVIDUAL P3/js/ag3.js
@@ -0,0 +1,50 @@
+function guardarDatos(){
+var nombre=document.getElementById("nombre").value;
+var movil=document.getElementById("movil").value;
+var email=document.getElementById("email").value;
+const datos={
+    'movil': movil,
+    'email': email,
+}
+localStorage.setItem(nombre, JSON.stringify(datos));
+document.getElementById("nombre").value="";
+document.getElementById("movil").value="";
+document.getElementById("email").value="";
+actualizarDatos();
+}
+
+function recuperarDatos(){
+    var nombre=document.getElementById("nombre").value;
+    var datos = localStorage.getItem(nombre);
+    datos = JSON.parse(datos);
+    document.getElementById("movil").value=datos.movil;
+    document.getElementById("email").value=datos.email;   
+}
+
+function eliminarDato(){
+    var nombre=document.getElementById("nombre").value;
+    localStorage.removeItem(nombre);
+    actualizarDatos();   
+}
+function eliminarTodo(){
+    localStorage.clear();
+    actualizarDatos();
+}
+function actualizarDatos(){
+    var registro="";
+    if(localStorage.length === 0){
+        registro+='<li>Vacio</li>';
+    }else{
+        for(var i=0;i<= localStorage.length-1; i++){
+            var key=localStorage.key(i);
+            var datos = localStorage.getItem(key);
+            datos= JSON.parse(datos);
+
+            registro += '<li>' + '<span class="nom">' + key + '</span>' 
+            + '<span class="nom">'+ datos.movil+ '</span>'
+            + '<span class="nom">'+ datos.email+ '</span>'+ '</li><br>';
+
+        }
+    }
+    document.getElementById('contactos').innerHTML=registro;
+}
