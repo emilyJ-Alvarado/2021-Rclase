b617de0e073dc21d2c98ec3d038b02c4f6d195ec
emilyAlvarado <emilyjhas@gmail.com>
2021-07-07 20:11:14 -0500

    07-07

---
+++ Emi/Belle Scarpe 1/Js/compra.js
@@ -0,0 +1,12 @@
+const compra=new Carrito();
+const listaCompra= document.querySelector('#lista-compra tbody');
+const carrito = document.getElementById('carrito');
+
+cargarEventos();
+
+function cargarEventos(){
+document.addEventListener('DOMContentLoaded', compra.leerLocalStorageCompra());
+carrito.addEventListener('click', (e)=>(compra.eliminarProducto(e)));
+
+compra.calcularTotal();
+}
